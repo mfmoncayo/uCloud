b1798cedead9c3b107723c6dc12444138a1021ed
Michael Moncayo <michaelmoncayo@Bond.local>
2017-01-15 16:02:18 -0600

    Initial Heroku

--- lifecloud/settings/production.py
+++ lifecloud/settings/production.py
@@ -28,7 +28,7 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
 #Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
