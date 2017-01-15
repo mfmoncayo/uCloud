54a69a0a247bc6dc42a50d43f46bd827de202c93
Michael Moncayo <michaelmoncayo@Bond.local>
2017-01-15 16:06:38 -0600

    Initial Heroku

--- lifecloud/settings/production.py
+++ lifecloud/settings/production.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from .base import *
 
-ALLOWED_HOSTS = ['lifecloud.herokuapp.com']
-
 import dj_database_url
 # Required function for Heroku to parse the DB variables
 DATABASES['default'] =  dj_database_url.config()
