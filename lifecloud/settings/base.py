e3d8259e076158f210753b5d4255ea469d323857
Michael Moncayo <michaelmoncayo@Bond.local>
2017-01-15 15:36:19 -0600

    Initial Heroku

--- lifecloud/settings/base.py
+++ lifecloud/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import django.contrib.auth
-import dj_database_url
 
 from django.core.exceptions import ImproperlyConfigured
 
--- lifecloud/settings/production.py
+++ lifecloud/settings/production.py
@@ -3,6 +3,7 @@ from .base import *
 
 ALLOWED_HOSTS = ['lifecloud.herokuapp.com']
 
+import dj_database_url
 # Required function for Heroku to parse the DB variables
 DATABASES['default'] =  dj_database_url.config()
 
