3125a92bf841141679c39b1520d8c1411911747f
Michael Moncayo <michaelmoncayo@Bond.local>
2017-01-15 15:38:05 -0600

    Initial Heroku

--- lifecloud/settings/production.py
+++ lifecloud/settings/production.py
@@ -6,5 +6,3 @@ ALLOWED_HOSTS = ['lifecloud.herokuapp.com']
 import dj_database_url
 # Required function for Heroku to parse the DB variables
 DATABASES['default'] =  dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = (HTTP_X_FORWARDED_PROTO, https)
