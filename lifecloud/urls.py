c7d33c061ba2c5c5e48b5938041a6c550e3ffb9d
Michael Moncayo <michaelmoncayo@Bond.local>
2017-01-24 20:00:00 -0600

    Apps Update

--- lifecloud/urls.py
+++ lifecloud/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('lifecloud.apps.welcome.urls')),
     url(r'^dashboard/', include('lifecloud.apps.dashboard.urls')),
+    url(r'^finance/', include('lifecloud.apps.finance.urls')),
+    url(r'^education/', include('lifecloud.apps.education.urls')),
+    url(r'^health/', include('lifecloud.apps.health.urls')),
+    url(r'^schedule/', include('lifecloud.apps.schedule.urls')),
 ]
