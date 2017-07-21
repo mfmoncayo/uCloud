c050c0f8360023098db50cbb5bf2d267ebc3ca94
Michael Moncayo <michaelmoncayo@Bond.local>
2017-07-20 19:23:37 -0500

    Prior Github Repository

--- lifecloud/static/dist/js/dashboard.js
+++ lifecloud/static/dist/js/dashboard.js
@@ -4,20 +4,4 @@ $(document).ready(function() {
   }).mouseout(function () {
     $(this).attr('src', '../../static/dist/images/logo.png');
   });
-
-  $(".exploreRightBtn").click(function () {
-    //FINISH ADDING JQUERY TO SHOW PLUS BTN on EXPLORE >>>
-    var attr = $(".exploreRightBtn2").attr('display');
-    if(attr==="undefined"){
-      $(".exploreRight").addClass('exploreRight2');
-    };
-    else(){
-      $(".exploreRight").addClass('exploreRight2');
-    };
-  });
-
-  $('.datepicker').datepicker({
-    startDate: '-3d'
-  });
-
 });
--- lifecloud/staticfiles/dist/js/dashboard.a26dd72da2f7.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".logo").mouseover(function () {
-    $(this).attr('src', '../../static/dist/images/logo2.png');
-  }).mouseout(function () {
-    $(this).attr('src', '../../static/dist/images/logo.png');
-  });
-});
