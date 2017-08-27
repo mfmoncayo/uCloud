2f9062ff8ead09c10eb882d5dd6a336117537ef8
michael moncayo <michaelmoncayo@cayo.local>
2017-08-26 21:41:28 -0500

    Portrait Update 4

--- ucloud/apps/welcome/static/js/welcome.js
+++ ucloud/apps/welcome/static/js/welcome.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
   $(".inactiveApplet").hover(
     function(){
-      $(".inactiveApplet").css("border-bottom","2px solid rgba(255, 243, 236, 0.25);").css("text-decoration","none");
+      $(".inactiveApplet").css("border-bottom","2px solid rgba(255, 243, 236, 0.25)").css("text-decoration","none");
     }, function(){
       $(".inactiveApplet").css("border-bottom","none");
     }
