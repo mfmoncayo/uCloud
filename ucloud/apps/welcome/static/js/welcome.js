ffa78423f4539d053d11e28d27e85ef823d3cfd6
Michael Moncayo <michaelmoncayo@bond.local>
2017-08-31 21:25:51 -0500

    land home view

--- ucloud/apps/welcome/static/js/welcome.js
+++ ucloud/apps/welcome/static/js/welcome.js
@@ -15,58 +15,58 @@ $(document).ready(function(){
     }
   );
 
-  $("#leftTab").click(function(){
-    $("#registerForm").show();
-    $("#loginForm").hide();
-    $("#forgotForm").hide();
+  $(".leftTab").click(function(){
+    $(".registerForm").show();
+    $(".loginForm").hide();
+    $(".forgotForm").hide();
 
-    $("#leftTab").addClass("active activeTab");
-    $("#leftTab").removeClass("inactiveTab");
-    $("#leftTab a").attr('id', 'currentTab');
+    $(".leftTab").addClass("active activeTab");
+    $(".leftTab").removeClass("inactiveTab");
+    $(".leftTab a").attr('id', 'currentTab');
 
-    $("#middleTab").addClass("inactiveTab");
-    $("#middleTab").removeClass("active activeTab");
-    $("#middleTab a").removeAttr('id');
+    $(".middleTab").addClass("inactiveTab");
+    $(".middleTab").removeClass("active activeTab");
+    $(".middleTab a").removeAttr('id');
 
-    $("#rightTab").addClass("inactiveTab");
-    $("#rightTab").removeClass("active activeTab");
-    $("#rightTab a").removeAttr('id');
+    $(".rightTab").addClass("inactiveTab");
+    $(".rightTab").removeClass("active activeTab");
+    $(".rightTab a").removeAttr('id');
   });
 
-  $("#middleTab").click(function(){
-    $("#registerForm").hide();
-    $("#loginForm").show();
-    $("#forgotForm").hide();
+  $(".middleTab").click(function(){
+    $(".registerForm").hide();
+    $(".loginForm").show();
+    $(".forgotForm").hide();
 
-    $("#leftTab").addClass("inactiveTab");
-    $("#leftTab").removeClass("active activeTab");
-    $("#leftTab a").removeAttr('id');
+    $(".leftTab").addClass("inactiveTab");
+    $(".leftTab").removeClass("active activeTab");
+    $(".leftTab a").removeAttr('id');
 
-    $("#middleTab").addClass("active activeTab");
-    $("#middleTab").removeClass("inactiveTab");
-    $("#middleTab a").attr('id', 'currentTab');
+    $(".middleTab").addClass("active activeTab");
+    $(".middleTab").removeClass("inactiveTab");
+    $(".middleTab a").attr('id', 'currentTab');
 
-    $("#rightTab").addClass("inactiveTab");
-    $("#rightTab").removeClass("active activeTab");
-    $("#rightTab a").removeAttr('id');
+    $(".rightTab").addClass("inactiveTab");
+    $(".rightTab").removeClass("active activeTab");
+    $(".rightTab a").removeAttr('id');
   });
 
-  $("#rightTab").click(function(){
-    $("#registerForm").hide();
-    $("#loginForm").hide();
-    $("#forgotForm").show();
+  $(".rightTab").click(function(){
+    $(".registerForm").hide();
+    $(".loginForm").hide();
+    $(".forgotForm").show();
 
-    $("#leftTab").addClass("inactiveTab");
-    $("#leftTab").removeClass("active activeTab");
-    $("#leftTab a").removeAttr('id');
+    $(".leftTab").addClass("inactiveTab");
+    $(".leftTab").removeClass("active activeTab");
+    $(".leftTab a").removeAttr('id');
 
-    $("#middleTab").addClass("inactiveTab");
-    $("#middleTab").removeClass("active activeTab");
-    $("#middleTab a").removeAttr('id');
+    $(".middleTab").addClass("inactiveTab");
+    $(".middleTab").removeClass("active activeTab");
+    $(".middleTab a").removeAttr('id');
 
-    $("#rightTab").addClass("active activeTab");
-    $("#rightTab").removeClass("inactiveTab");
-    $("#rightTab a").attr('id', 'currentTab');
+    $(".rightTab").addClass("active activeTab");
+    $(".rightTab").removeClass("inactiveTab");
+    $(".rightTab a").attr('id', 'currentTab');
   });
 
   $("input[name='email']").parent().css("margin-bottom","0");
