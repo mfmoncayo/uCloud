c403a02bfe430036df3cde2bd6a081466a2c47c9
Michael Moncayo <michaelmoncayo@bond.local>
2017-09-09 20:54:58 -0500

    Discover it

--- ucloud/static/dist/js/discover.js
+++ ucloud/static/dist/js/discover.js
@@ -9,15 +9,38 @@ $(document).ready(function() {
   $(".navWidthLeft2").css("-webkit-flex","0 0 12%");
   $(".navWidthLeft2").css("margin-right","4.66667%");
 
-  $(".hoveringBtn").mouseover(function () {
-    $(this).css('background', 'rgba(0, 0, 0, .6)');
+$(".tabColorActive").css("color","rgba(255,255,255,.8)");
+  $(".tabColor").css("color","rgba(255,255,255,.5)");
+  $(".tabColor").mouseover(function () {
+    $(this).css("color","rgba(255,255,255,.8)");
   }).mouseout(function () {
-    $(this).css('background', 'rgba(0, 0, 0, .4)');
+    $(this).css("color","rgba(255,255,255,.5)");
+  });
+
+
+  $(".nav-link.specialTabs").css("background","rgba(0,0,0,.4)");
+  $(".nav-link.specialTabs").css("border-color","rgba(0,0,0,.6)");
+
+  $(".nav-link.active").css("background","rgba(0,0,0,.6)");
+  $(".nav-link.active").css("border-color","rgba(0,0,0,.6)");
+
+  $(".hoveringBtn").mouseover(function () {
+    if($(this).css('display','block')) {
+      $(".hoveringBtn").mouseover(function () {
+        $(this).css('background', 'rgba(0, 0, 0, .6)');
+      }).mouseout(function () {
+        $(this).css('background', 'rgba(0, 0, 0, .4)');
+      });
+    }
+    else{
+    };
   });
 
   $(".explore").click(function () {
     if($('.col-4').hasClass('collapseNavFixLeft')) {
-      $(".collapseNavFix").css('background','rgba(0,0,0,.4)');
+      $(".noDisplaySubNav").css('display','block');
+      $(".collapseNavFix").addClass('hoveringBtn');
+      $(".collapseNavFix").css('background-color','rgba(0,0,0,.4)');
       $(".collapseNavFix").css('transition','.5s linear');
       $(".collapseNavFix").css('box-shadow','0px 0px 5px #888888');
       $(".collapseNavFixLeft").attr('class', 'col-4 fixLeft');
@@ -26,10 +49,12 @@ $(document).ready(function() {
       $(".fixLeft").attr('class', 'col-4 collapseNavFixLeft');
       $(".collapseNavFix").css('background','none');
       $(".collapseNavFix").css('box-shadow','none');
+      $(".collapseNavFix").removeClass('hoveringBtn');
     }
   });
 
-  $(".footerWidthLeft2").css("width","12%");
-  $(".footerWidthLeft2").css("-webkit-flex","0 0 12%");
   $(".profFix").css("margin-right","0px");
+  $(".leaflet-control-attribution").css("margin-right","615px");
+  $(".leaflet-left > .leaflet-control-zoomhome").css("margin-bottom","28px");
+
 });
--- ucloud/static/dist/js/leaflet.zoomhome.js
+++ ucloud/static/dist/js/leaflet.zoomhome.js
@@ -57,4 +57,56 @@
     L.Control.zoomHome = function (options) {
         return new L.Control.ZoomHome(options);
     };
+
+    L.Control.ZoomHome2 = L.Control.Zoom.extend({
+        options: {
+            position: 'bottomleft',
+            zoomInText: 'cloud',
+            zoomInTitle: 'New Cloud',
+            zoomCloudIcon: 'cloud',
+            zoomCloudTitle: 'New Cloud',
+            zoomOutText: '/',
+            zoomOutTitle: 'Close Cloud',
+            zoomHomeIcon: 'star',
+            zoomHomeTitle: 'Save Cloud',
+            homeCoordinates: null,
+            homeZoom: null
+        },
+
+        onAdd: function (map) {
+            var controlName = 'leaflet-control-zoomhome',
+                container = L.DomUtil.create('div', controlName + ' leaflet-bar'),
+                options = this.options;
+
+            if (options.homeCoordinates === null) {
+                options.homeCoordinates = map.getCenter();
+            }
+            if (options.homeZoom === null) {
+                options.homeZoom = map.getZoom();
+            }
+
+            var zoomCloudText = '<i class="fa fa-' + options.zoomCloudIcon + '" style="line-height:1.65;"></i>';
+            this._zoomInButton = this._createButton(zoomCloudText, options.zoomInTitle,
+                controlName + '-in', container, this._zoomIn.bind(this));
+            var zoomHomeText = '<i class="fa fa-' + options.zoomHomeIcon + '" style="line-height:1.65;"></i>';
+            this._zoomHomeButton = this._createButton(zoomHomeText, options.zoomHomeTitle,
+                controlName + '-home', container, this._zoomHome2.bind(this));
+            this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
+                controlName + '-out', container, this._zoomOut.bind(this));
+
+            this._updateDisabled();
+            map.on('zoomend zoomlevelschange', this._updateDisabled, this);
+
+            return container;
+        },
+
+        _zoomHome2: function (e) {
+            //jshint unused:false
+            this._map.setView(this.options.homeCoordinates, this.options.homeZoom);
+        }
+    });
+
+    L.Control.zoomHome2 = function (options) {
+        return new L.Control.ZoomHome2(options);
+    };
 }());
