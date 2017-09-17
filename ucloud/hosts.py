e13f61571eed4f01d829c9dba6393137cfa4d87b
Michael Moncayo <michaelmoncayo@bond.local>
2017-09-17 12:19:35 -0500

    sub update

--- ucloud/hosts.py
+++ ucloud/hosts.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name='www'),
     host(r'discover', 'ucloud.apps.discover.urls', name='discover'),
     #host(r'(?!www).*', 'ucloud.settings.hosts.urls', name='wildcard_redirect'),
 )
