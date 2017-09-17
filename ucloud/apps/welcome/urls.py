85fc9fb26f2b79b7a9398880a226824eafc2eeb8
Michael Moncayo <michaelmoncayo@bond.local>
2017-09-17 12:28:58 -0500

    sub update

--- ucloud/apps/welcome/urls.py
+++ ucloud/apps/welcome/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import HomeView
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view()),
+    url(r'^$', HomeView.as_view(), name='www'),
 ]
--- ucloud/hosts.py
+++ ucloud/hosts.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django_hosts import patterns, host
 
-host_patterns = patterns('',
-    host(r'discover', 'ucloud.apps.discover.urls', name='discover'),
+host_patterns = patterns('ucloud.apps',
+    host(r'www', 'welcome.urls', name='www'),
+    host(r'discover', 'discover.urls', name='discover'),
     #host(r'(?!www).*', 'ucloud.settings.hosts.urls', name='wildcard_redirect'),
 )
