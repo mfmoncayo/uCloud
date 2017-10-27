36e7291ee8b4a3d1695037f80c9712f09be1639c
Michael Moncayo <michaelmoncayo@bond.local>
2017-10-27 18:20:14 -0500

    redirect

--- ucloud/apps/welcome/views.py
+++ ucloud/apps/welcome/views.py
@@ -34,7 +34,7 @@ class HomeView(View):
 
             if user is not None:
                 login(request, user)
-                return redirect('journey', host='journey')
+                return redirect('journey')
 
             context = {
                 "loginform": loginform,
