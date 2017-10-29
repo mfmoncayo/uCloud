d26f82328ba299ced6092a5665a086c3ada51fdc
Michael Moncayo <michaelmoncayo@bond.local>
2017-10-28 22:04:11 -0500

    user auth

--- ucloud/apps/journey/views.py
+++ ucloud/apps/journey/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
-from django_hosts.resolvers import reverse
 from django.views import View
+from django.shortcuts import render, redirect
+from django_hosts.resolvers import reverse
+from django.http import HttpResponse
 from .forms import StoryForm
 
 # Create your views here.
 class JourneyView(View):
-    def get(self, request, *args, **kwargs):]
+    def get(self, request, *args, **kwargs):
         start_storyform = StoryForm()
 
         context = {
