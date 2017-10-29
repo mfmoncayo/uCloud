664e69a376da196c1689cd3dc475c351ac2551a4
Michael Moncayo <michaelmoncayo@bond.local>
2017-10-28 21:23:00 -0500

    user auth

--- ucloud/apps/journey/views.py
+++ ucloud/apps/journey/views.py
@@ -1,15 +1,20 @@
-from django.views import View
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django_hosts.resolvers import reverse
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.views import View
 from .forms import StoryForm
 
 # Create your views here.
 class JourneyView(View):
     def get(self, request, *args, **kwargs):
-        start_storyform = StoryForm()
+        if request.user.is_authenticated():
+            start_storyform = StoryForm()
 
-        context = {
-            "storyform": start_storyform,
-        }
-        return render(request, 'journey.html', context)
+            context = {
+                "storyform": start_storyform,
+                "user": User.objects.get(username='john')
+            }
+            return render(request, 'journey.html', context)
+        else:
+            return HttpResponseRedirect('http://www.ucloud.live')
